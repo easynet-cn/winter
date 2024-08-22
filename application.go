20f9213891fd70a156458500b51080e689b4645a
liangtianyu <124244236@qq.com>
2024-08-22 11:16:56 +0800

    直接初始化nacos，数据库，redis

--- application.go
+++ application.go
@@ -4,33 +4,85 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 type ApplicationFunc func()
 
 type Application struct {
-	engine *gin.Engine
-	nacos  *Nacos
+	engine   *gin.Engine
+	nacos    *Nacos
+	logger   *zap.Logger
+	database *Database
+	redis    *Redis
 }
 
 func NewApplication(
-	nacos *Nacos,
-	systemMiddleware *SystemMiddleware,
+	metadata map[string]string,
+	version string,
+	syncDBFunc func() error,
 ) *Application {
+	nacos := NewNacos(metadata)
+
+	nacos.Init()
+
+	logger := NewLogger(nacos.GetConfig())
+
 	gin.SetMode(gin.ReleaseMode)
 
+	engine := gin.Default()
+
 	application := &Application{
-		engine: gin.Default(),
+		engine: engine,
 		nacos:  nacos,
+		logger: logger,
 	}
 
-	RegisterDefaultMiddleware(application.engine, systemMiddleware)
+	application.nacos.Init()
+	application.logger = NewLogger(nacos.GetConfig())
+
+	database := NewDatabase(nacos.GetConfig())
+
+	database.Init()
+
+	application.database = database
+
+	redis := NewRedis(nacos.GetConfig())
+
+	redis.Init()
+
+	application.redis = redis
+
+	RegisterDefaultMiddleware(
+		application.engine,
+		&SystemMiddleware{
+			Logger:     application.logger,
+			Config:     nacos.GetConfig(),
+			Version:    version,
+			SyncDBFunc: syncDBFunc,
+		})
 
 	return application
 }
 
 func (m *Application) GetEngine() *gin.Engine {
 	return m.engine
+}
+
+func (m *Application) GetNacos() *Nacos {
+	return m.nacos
+}
+
+func (m *Application) GetLogger() *zap.Logger {
+	return m.logger
+}
+
+func (m *Application) GetDatabase() *Database {
+	return m.database
+}
+
+func (m *Application) GetRedis() *Redis {
+	return m.redis
 }
 
 func (m *Application) Run(funcs ...ApplicationFunc) {
