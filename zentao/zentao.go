de6186764b9e36c8b6630da7425e2176e764f6cc
liangtianyu <124244236@qq.com>
2024-09-09 19:21:30 +0800

    修正禅道项目创建人类型

--- zentao/zentao.go
+++ zentao/zentao.go
@@ -85,20 +85,20 @@ type SimpleUser struct {
 }
 
 type Project struct {
-	Id         int     `json:"id"`         // 项目ID
-	Name       string  `json:"name"`       // 项目名称
-	Code       string  `json:"code"`       // 项目编号
-	Model      string  `json:"model"`      // 项目模型(scrum敏捷 | waterfall 瀑布)
-	Budget     string  `json:"budget"`     // 项目预算
-	BudgetUnit string  `json:"budgetUnit"` // 预算币种(CNY | USD)
-	Parent     int     `json:"parent"`     // 所属项目集
-	Begin      *string `json:"begin"`      // 预计开始日期
-	End        *string `json:"end"`        // 预计结束日期
-	Status     string  `json:"status"`     // 项目状态(wait 未开始 | doing 进行中 | suspend 已挂起 | closed 已关闭)
-	OpenedBy   string  `json:"openedBy"`   // 创建人
-	OpenedDate string  `json:"openedDate"` // 创建时间
-	PM         string  `json:"pm"`         // 项目经理
-	Progress   string  `json:"progress"`   // 进度
+	Id         int         `json:"id"`         // 项目ID
+	Name       string      `json:"name"`       // 项目名称
+	Code       string      `json:"code"`       // 项目编号
+	Model      string      `json:"model"`      // 项目模型(scrum敏捷 | waterfall 瀑布)
+	Budget     string      `json:"budget"`     // 项目预算
+	BudgetUnit string      `json:"budgetUnit"` // 预算币种(CNY | USD)
+	Parent     int         `json:"parent"`     // 所属项目集
+	Begin      *string     `json:"begin"`      // 预计开始日期
+	End        *string     `json:"end"`        // 预计结束日期
+	Status     string      `json:"status"`     // 项目状态(wait 未开始 | doing 进行中 | suspend 已挂起 | closed 已关闭)
+	OpenedBy   *SimpleUser `json:"openedBy"`   // 创建人
+	OpenedDate string      `json:"openedDate"` // 创建时间
+	PM         string      `json:"pm"`         // 项目经理
+	Progress   string      `json:"progress"`   // 进度
 }
 
 type ProjectPageResult struct {
