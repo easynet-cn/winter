e440b0cefa10b438efd3525d01cb7e88ab5e2671
liangtianyu <124244236@qq.com>
2024-09-09 16:15:09 +0800

    禅道部门增加子部门

--- zentao/zentao.go
+++ zentao/zentao.go
@@ -43,15 +43,16 @@ type GetTokenResponse struct {
 }
 
 type Department struct {
-	Id       int    `json:"id"`       // 部门编号
-	Name     string `json:"name"`     // 部门名称
-	Parent   int    `json:"parent"`   // 上级部门
-	Path     string `json:"path"`     // 路径
-	Grade    int    `json:"grade"`    // 部门级别
-	Order    int    `json:"order"`    // 排序
-	Position string `json:"position"` // 职位
-	Function string `json:"function"` // 职能
-	Manager  string `json:"manager"`  // 负责人
+	Id       int          `json:"id"`       // 部门编号
+	Name     string       `json:"name"`     // 部门名称
+	Parent   int          `json:"parent"`   // 上级部门
+	Path     string       `json:"path"`     // 路径
+	Grade    int          `json:"grade"`    // 部门级别
+	Order    int          `json:"order"`    // 排序
+	Position string       `json:"position"` // 职位
+	Function string       `json:"function"` // 职能
+	Manager  string       `json:"manager"`  // 负责人
+	Children []Department `json:"children"` // 子部门
 }
 
 type UserProfile struct {
