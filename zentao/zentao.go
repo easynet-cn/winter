4e8e595f5d39d536bc1919c8488c8bacca5a0bed
liangtianyu <124244236@qq.com>
2024-09-10 08:35:25 +0800

    修正禅道需求创建人类型

--- zentao/zentao.go
+++ zentao/zentao.go
@@ -124,22 +124,22 @@ type ExecutionPageResult struct {
 }
 
 type Story struct {
-	Id         int    `json:"id"`         // 需求ID
-	Product    int    `json:"product"`    // 所属产品
-	Branch     int    `json:"branch"`     // 所属分支
-	Module     int    `json:"module"`     // 所属产品模块
-	FromBug    int    `json:"fromBug"`    // 来自于Bug
-	Source     string `json:"source"`     // 需求来源(customer 客户 | user 用户 | po 产品经理 | market 市场)
-	SourceNo   string `json:"sourceNo"`   // 来源备注
-	Title      string `json:"title"`      // 需求标题
-	Category   string `json:"category"`   // 类型(feature 功能 | interface 接口 | performance 性能 | safe 安全 | experience 体验 | improve 改进 | other 其他)
-	Stage      string `json:"stage"`      // 阶段(wait 未开始 | planned 已计划 | projected 已立项 | developing 研发中 | developed 研发完毕 | testing 测试中 | tested 测试完毕 | verified 已验收 | released 已发布 | closed 已关闭)
-	Pri        int    `json:"pri"`        // 优先级
-	Estimate   int    `json:"estimate"`   // 预计工时
-	Status     string `json:"status"`     // 状态(draft 草稿 | active 激活 | closed 已关闭 | changed 已变更)
-	OpenedBy   string `json:"openedBy"`   // 创建人
-	OpenedDate string `json:"openedDate"` // 创建时间
-	ToBug      int    `json:"toBug"`      // 转为Bug
+	Id         int         `json:"id"`         // 需求ID
+	Product    int         `json:"product"`    // 所属产品
+	Branch     int         `json:"branch"`     // 所属分支
+	Module     int         `json:"module"`     // 所属产品模块
+	FromBug    int         `json:"fromBug"`    // 来自于Bug
+	Source     string      `json:"source"`     // 需求来源(customer 客户 | user 用户 | po 产品经理 | market 市场)
+	SourceNo   string      `json:"sourceNo"`   // 来源备注
+	Title      string      `json:"title"`      // 需求标题
+	Category   string      `json:"category"`   // 类型(feature 功能 | interface 接口 | performance 性能 | safe 安全 | experience 体验 | improve 改进 | other 其他)
+	Stage      string      `json:"stage"`      // 阶段(wait 未开始 | planned 已计划 | projected 已立项 | developing 研发中 | developed 研发完毕 | testing 测试中 | tested 测试完毕 | verified 已验收 | released 已发布 | closed 已关闭)
+	Pri        int         `json:"pri"`        // 优先级
+	Estimate   int         `json:"estimate"`   // 预计工时
+	Status     string      `json:"status"`     // 状态(draft 草稿 | active 激活 | closed 已关闭 | changed 已变更)
+	OpenedBy   *SimpleUser `json:"openedBy"`   // 创建人
+	OpenedDate string      `json:"openedDate"` // 创建时间
+	ToBug      int         `json:"toBug"`      // 转为Bug
 }
 
 type StoryPageResult struct {
